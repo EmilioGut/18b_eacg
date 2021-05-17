cfea1ccb2ce4c2c0d5f50649dce63e7c0ea3234c
EmilioGut <emiliocampregher@yahoo.com.br>
2021-05-17 17:41:47 -0300

    Primeiro Commit do Sistema

---
+++ index.js
@@ -0,0 +1,11 @@
+$(() => {
+    $("#btnAdionar").click(() =>{
+        $("#tblDados tbody").append(" <tr>
+        <td>-- # --<td>
+        <td>Nome</td>
+        <td>- sobrenome -</td>
+        <td>01/01/1980</td> </tr>")
+    })
+
+
+})
